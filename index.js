cf366f522df5bc55948121157b2ee9e59776861c
Isaiah <zaymgmt707@gmail.com>
2024-07-26 20:26:24 -0700

    updates iindex.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+  charArray = string.split(``)
+  reverseCharArray = charArray.reverse()
+  reversedString = reverseCharArray.join(``)
+  return string === reversedString
 }
 
 /* 
-  Add your pseudocode here
+define a fucntion isPalindrome takes a string as its argument
+split the string
+reverse the string
+join it together
+compare
 */
 
 /*
-  Add written explanation of your solution here
+is palindrome takes a string as its parameter
+it splits the string into individual letters
+then it reverses those letters
+then it joins the reversed string together
+if reversed string === the argument return true else false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log(isPalindrome("racecar"))
 }
 
 module.exports = isPalindrome;
